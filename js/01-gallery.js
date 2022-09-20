f9eda2e3594748d2774374d98cb1320317a64d68
Tanets Alexandr <104691488+AlexTanetsDev@users.noreply.github.com>
2022-09-20 20:18:17 +0300

    finish HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map((item) => creteGalleryMarkup(item))
+  .join("");
+
+function creteGalleryMarkup({ preview, original, description }) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+}
+
+galleryContainerRef.innerHTML = galleryMarkup;
+
+galleryContainerRef.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img width="1400" height="900" src=${evt.target.dataset.source}>`
+  );
+
+  instance.show();
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.code !== "Escape" || !instance.visible()) {
+      return;
+    }
+    instance.close();
+  });
+});
